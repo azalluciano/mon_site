566471d2d98041fa29a3e4befdb66bbc9930e5a2
Azal Luciano <lazaluciano327@gmail.com>
2022-08-10 17:05:32 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var typed = new Typed('.typed', {
+    strings: ["Bonjour à tous et à toutes, ceci est mon site officiel de portefeuille contenant tout mon travail de développement web et mobile. Je suis un jeune étudiant originaire de Toamasina.Passionné par la technologie de l'information. Je suis une personne motivée et dévouée dans tous ce que je fait. J'ai décidé de me lancer à fond dans l'apprentissage du code. Plus qu'un avenir professionel, je me suis decouvert une passion, celle de coder. "],
+    typeSpeed: 20,
+  });
